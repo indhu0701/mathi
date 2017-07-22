7b5b7a2d548adea00692be8c318309e150a32ff0
indhu0701 <30342676+indhu0701@users.noreply.github.com>
2017-07-22 10:42:14 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int num=5;
+if (num>0)
+{
+printf("the number is positve");
+}
+else if(num<0)
+{
+printf("the number is negative");
+}
+else
+{
+printf("the number is zero");
+}
+return0;
+}
