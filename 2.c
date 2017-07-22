eeecb589df1d3d55afc87e3c60a979ccc46ac11c
indhu0701 <30342676+indhu0701@users.noreply.github.com>
2017-07-22 11:11:53 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int number;
+{
+printf("enter the number");
+}
+if(number%2==0)
+{
+printf("%d is even",number);
+}
+else
+{
+printf("%d is odd",nuber);
+}
+return 0;
+}
